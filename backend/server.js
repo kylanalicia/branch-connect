7164ef8b169be2798c95003dd51fa6a47d4760a2
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-07 17:49:40 +0300

    managed my server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-const express = require('express')
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT  || 5000;
+app.get('/', (req, res) => {
+    // root route http://localhost:5000/
+    res.send("Hello World!!");
+});
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
