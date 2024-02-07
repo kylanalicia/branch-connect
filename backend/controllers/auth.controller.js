94f0db049c210ca396f1fef7fb90d0597411f034
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-07 18:17:15 +0300

    created the routes for the login,logout and signup function

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res, next) => {
+    console.log('loginUser');
+}
+
+export const logout = (req, res, next) => {
+    console.log('logoutUser');
+}
+
+export const signup = (req, res, next) => {
+    console.log('signupUser');
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
@@ -9,5 +11,7 @@ app.get('/', (req, res) => {
     // root route http://localhost:5000/
     res.send("Hello World!!");
 });
+
+app.use("/api/auth",authRoutes);
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
