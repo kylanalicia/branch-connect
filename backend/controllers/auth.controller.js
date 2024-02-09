0e218fbf40cda4b5536a7ada67db2771ff197b07
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-09 09:02:24 +0300

    worked on the login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -58,7 +58,27 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-    // Login logic
+	try {
+		const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: "Invalid username or password" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 };
 
 export const logout = (req, res, next) => {
