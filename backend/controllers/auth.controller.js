6a2c4805a16a5a9d3bc7a05e9d8d8f9db2d149b6
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-09 08:03:32 +0300

    made changes in the authcontroller file to make the code cleaner

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -18,7 +18,7 @@ export const signup = async (req, res) => {
         // HASH PASSWORD HERE
         const salt = await bcryptjs.genSalt(10);
         const hashedPassword = await bcryptjs.hash(password, salt);
-        
+
         // https://avatar-placeholder.iran.liara.run/
 
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -32,6 +32,7 @@ export const signup = async (req, res) => {
             profilePic: gender === 'male' ? boyProfilePic : girlProfilePic 
         });
 
+        if (newUser) {
         await newUser.save();
 
         res.status(200).json({
@@ -40,6 +41,12 @@ export const signup = async (req, res) => {
             username: newUser.username,
             profilePic: newUser.profilePic
         });
+
+    } else {
+        res.staus(400).json({ error: "Invalid user data"});
+    }
+
+       
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
