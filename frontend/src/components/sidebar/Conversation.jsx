0fd2b1e17b67f35a3bd6dbe67a6a3e866e5a7c45
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-11 08:40:02 +0300

    handled the get conversation

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,24 +1,34 @@
-const Conversation = () => {
-  return <>
-  <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursoe-pointer">
-    <div className="avatar online">
-      <div className="w-12 rounded-full">
-        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="user avatar" />
-      </div>
-    </div>
-    <div className="flex flex-col flex-1">
-      <div className="flex gap-3 justify-between">
-        <p className="font-bold text-gray-200">Tiffany Haddish </p>
-        <span className="text-xl"></span>
-      </div>
+import useConversation from "../../zustand/useConversation";
 
-    </div>
-  </div>
-  <div className="divider my-0 py-0 h-1"></div>
-  </>;
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const {selectedConversation, setSelectedConversation} = useConversation();
+  const isSelected = selectedConversation?._id === conversation.id;
+  return (
+    <>
+      <div className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+      ${isSelected ? "bg-sky-500" : ""}
+      `}
+      onClick={()=>setSelectedConversation(conversation)}
+      >
+        <div className="avatar online">
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic} alt="user avatar" />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
+          </div>
+        </div>
+      </div>
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+    </>
+  );
 };
 
 export default Conversation;
+
 
 
 // STARTER CODE SNIPPET
--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,19 +1,33 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-  return (
-    <div className="py-2 flex flex-col overflow-auto">
-        <Conversation />
-        <Conversation />
-        <Conversation/>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-    </div>
-  )
+    const { loading, conversations } = useGetConversations();
+
+    console.log("CONVERSATIONS:", conversations);
+
+    return (
+        <div className='py-2 flex flex-col overflow-auto'>
+            {loading ? (
+                <span className='loading loading-spinner mx-auto'></span>
+            ) : conversations.length === 0 ? (
+                <span>No conversations found.</span>
+            ) : (
+                conversations.map((conversation) => (
+                    <Conversation
+                        key={conversation._id}
+                        conversation={conversation}
+                        emoji={getRandomEmoji()}
+                    />
+                ))
+            )}
+        </div>
+    );
 };
 
 export default Conversations;
+
 
 
 // STARTER CODE SNIPPET
