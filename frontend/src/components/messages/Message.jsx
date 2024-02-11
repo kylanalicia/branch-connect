9981b422b2bfbd3d888bc2ec4d52f58567e13f2a
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-11 09:53:30 +0300

    worked on the get messages functionality

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,28 @@
-const Message = () => {
-    return (
-      <div className="chat chat-end">
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img alt="Tailwind CSS chat bubble component"
-             src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" 
-             />
-          </div>
-        </div>
-        <div className={'chat-bubble text-white bg-blue-500'}>Hi,can I get the batch number?</div>
-        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">4:23</div>
-      </div>
-    );
-  }
-  
-  export default Message;
-  
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
+	// const shakeClass = message.shouldShake ? "shake" : "";
+
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white bg-blue-500 ${bubbleBgColor} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
+};
+export default Message;
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,21 +1,34 @@
+import { useEffect, useRef } from 'react'; // Import useRef for lastMessageRef
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
+
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+  const lastMessageRef = useRef(null); 
+  useEffect(() => {
+    setTimeout(() => {
+    lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+    }, 100);
+  },[messages]);
+
   return (
-    <div className="px-4 flex-1 overflow-auto">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+    <div className='px-4 flex-1 overflow-auto'>
+      {!loading && messages && messages.length > 0 && (
+        messages.map((message, index) => (
+          <div key={message._id} ref={index === messages.length - 1 ? lastMessageRef : null}>
+            <Message message={message} />
+          </div>
+        ))
+      )}
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      
+      {!loading && (!messages || messages.length === 0) && (
+        <p className='text-center'>Send a message to start the conversation</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Messages;
