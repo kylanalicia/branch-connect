904514d78f96a4dd9c78a2cef6fc399d60eb6ea3
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-11 15:12:39 +0300

    modified code

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -17,8 +17,7 @@ const Messages = () => {
 
     return (
         <div className='px-4 flex-1 overflow-auto'>
-            {!loading &&
-                messages.length > 0 &&
+            {messages && messages.length > 0 &&
                 messages.map((message, index) => (
                     <div key={message._id} ref={index === messages.length - 1 ? lastMessageRef : null}>
                         <Message message={message} />
@@ -26,7 +25,7 @@ const Messages = () => {
                 ))}
 
             {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-            {!loading && messages.length === 0 && (
+            {!loading && messages && messages.length === 0 && (
                 <p className='text-center'>Send a message to start the conversation</p>
             )}
         </div>
