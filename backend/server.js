4cd20ecc30b3fedbc26a9f9f666467452c99f430
aliciakimani.ky@gmail.com <aliciakimani.ky@gmail.com>
2024-02-11 14:19:54 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,34 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+
+
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from './db/connectToMongoDB.js';
-import { app, server } from './socket/socket.js';
-
-dotenv.config();
-
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware to parse JSON request bodies
+const __dirname = path.resolve();
+
+dotenv.config();
+
 app.use(express.json());
 app.use(cookieParser());
 
-// Route for authentication
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// Root route handler
-app.get('/', (req, res) => {
-    res.send("Hello World!!");
-});
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
